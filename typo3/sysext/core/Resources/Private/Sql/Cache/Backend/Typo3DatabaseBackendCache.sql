0f32e101212b618392c73cd37acc361baa7943c5
Frank Blank <65388000+frankblank@users.noreply.github.com>
2020-05-15 13:21:24 +0200

    Update Typo3DatabaseBackendCache.sql

--- typo3/sysext/core/Resources/Private/Sql/Cache/Backend/Typo3DatabaseBackendCache.sql
+++ typo3/sysext/core/Resources/Private/Sql/Cache/Backend/Typo3DatabaseBackendCache.sql
@@ -1,6 +1,6 @@
 CREATE TABLE ###CACHE_TABLE### (
 	id int(11) unsigned NOT NULL auto_increment,
-	identifier varchar(250) DEFAULT '' NOT NULL,
+	identifier varchar(500) DEFAULT '' NOT NULL,
 	expires int(11) unsigned DEFAULT '0' NOT NULL,
 	content longblob,
 	PRIMARY KEY (id),
